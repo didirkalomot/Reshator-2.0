4a816a87f7d25b40b139c22aaf2062d291895f5f
Alex Dizer <Alex_D.2005@mail.ru>
2025-07-29 17:13:41 +0700

    make_priblem 0.2

--- problem.py
+++ problem.py
@@ -61,7 +61,7 @@ class Letter(Value):
         return self.value
 
 class Operator(Node):
-    def __init__(self, priority = 1, left=None, right=None):
+    def __init__(self, priority, left=None, right=None):
         super().__init__(left, right)
         self.priority = priority
 
@@ -70,27 +70,14 @@ class Operator(Node):
 
     def work(self):pass
 
-class Plus(Operator):
-    def __init__(self, left, right):
-        super().__init__(1, left, right)
-
-    def __str__(self):
-        return '+'
-
-    def work(self):
-        try:
-            return self.left + self.right
-        except:
-            return None
-
 class Minus(Operator):
     def __init__(self, left, right=None):
         if right is None:
             right = left
             left = None
-            super().__init__(9, left, right)
+            super().__init__(1, left, right)
             return
-        super().__init__(1, left, right)
+        super().__init__(2, left, right)
 
     def __str__(self):
         return '-'
@@ -104,32 +91,19 @@ class Minus(Operator):
         except:
             return None
         
-class Mult(Operator):
+class Plus(Operator):
     def __init__(self, left, right):
         super().__init__(2, left, right)
 
     def __str__(self):
-        return '*'
+        return '+'
 
     def work(self):
         try:
-            return self.left * self.right
+            return self.left + self.right
         except:
             return None
-
-class Div(Operator):
-    def __init__(self, left, right):
-        super().__init__(2, left, right)
-
-    def __str__(self):
-        return '/'
-
-    def work(self):
-        try:
-            return self.left / self.right
-        except:
-            return None
-
+        
 class Pow(Operator):
     def __init__(self, left, right):
         super().__init__(3, left, right)
@@ -143,6 +117,32 @@ class Pow(Operator):
         except:
             return None
         
+class Mult(Operator):
+    def __init__(self, left, right):
+        super().__init__(4, left, right)
+
+    def __str__(self):
+        return '*'
+
+    def work(self):
+        try:
+            return self.left * self.right
+        except:
+            return None
+
+class Div(Operator):
+    def __init__(self, left, right):
+        super().__init__(4, left, right)
+
+    def __str__(self):
+        return '/'
+
+    def work(self):
+        try:
+            return self.left / self.right
+        except:
+            return None
+        
 #####################################################################################
 
 class Bad(Exception):
@@ -150,12 +150,12 @@ class Bad(Exception):
         self.current = current
         self.damage = damage
     
-class NotOperatorDefinition(Bad):
+class PriorityError(Bad):
     def __init__(self, current : Node):
-        super().__init__(current, 3)
+        super().__init__(current, 1)
 
     def __str__(self):
-        return f'нет оператора "{str(self.current)}" для классов: {type(self.current.left).__name__} и {type(self.current.right).__name__}'
+        return 'не тот порядок действий'
 
 #####################################################################################
 
@@ -174,7 +174,7 @@ class Problem():
     def print_tree(self):
         def recursive_print_tree(node: Node, depth = 0):
             if node is None: return
-            string = '-' * depth + type(node).__name__+ ' ' + str(node)
+            string = '-' * depth + str(node)
             print(string)
             recursive_print_tree(node.left, depth+1)
             recursive_print_tree(node.right, depth+1)
@@ -194,7 +194,7 @@ class Problem():
         if not isinstance(operator, Operator): 
             return     
         result = operator.work()
-        if result is None: raise NotOperatorDefinition(operator)
+        if result is None: raise PriorityError(operator)
         if operator == self.root:
             self.root = result 
         else:
@@ -207,14 +207,14 @@ class Problem():
 
 #####################################################################################
 
-symbols_operators_dict = {
+symbols_operators = {
     '+' : Plus,
     '-' : Minus,
     '*' : Mult,
     '/' : Div,
     '^' : Pow
 }
-
+'''
 def make_problem(string: str) -> Problem:
     def problem_string_to_list(string: str):
         nodes_list = string.split(' ')
@@ -223,10 +223,10 @@ def make_problem(string: str) -> Problem:
                 number = float(ch)
                 nodes_list[i] = Number(number)
             except:
-                if ch in symbols_operators_dict:
-                    nodes_list[i] = symbols_operators_dict[ch](None, None)
+                if ch in symbols_operators:
+                    nodes_list[i] = symbols_operators[ch](None, None)
                 elif ch.isalpha() and len(ch) == 1: nodes_list[i] = Letter(ch)
-                else: raise TypeError   
+        print(nodes_list)
         return nodes_list
 
     def find_root(nodes: list) -> Node:
@@ -252,23 +252,62 @@ def make_problem(string: str) -> Problem:
     nodes = problem_string_to_list(string)
     root = recursive_make_problem(nodes)
     return Problem(root)
-
+'''
 #####################################################################################
 
-problem = make_problem('- 33 / 5 + -4 * 3')
+def make_problem(string: str) -> Problem:
+    string = string.replace(' ', '')
+
+    def get_priority(char: str) -> int:
+        if char not in symbols_operators: return None
+        return symbols_operators[char](None, None).priority
+    
+    def find_root_index(string):
+        depth = 0
+        index = None
+        last_priority = None
+
+        for i, ch in enumerate(string):
+            if ch == '(': depth += 1; continue
+            if ch == ')': depth -= 1; continue
+            if depth != 0: continue
+            
+            priority = get_priority(ch)
+            if i == 0 and ch == '-': priority = 1
+            if priority != None:
+                if last_priority == None: last_priority = priority
+                if priority >= last_priority:
+                    last_priority = priority
+                    index = i
+        return index
+    
+    def make_value(string: str) -> Value:
+        try: return Number(float(string))
+        except: 
+            try: return Letter(string)
+            except: return None
+
+    def recursive_make_node(string: str) -> Node:
+        if string[0] == '(' and string[-1] == ')': string = string[1:-1]
+        if string == '' or ' ' or None: return None
+
+        value = make_value(string)
+        if value != None: return value
+
+        index = find_root_index(string)
+        char = string[index]
+        left = string[:index]
+        right = string[index+1:]
+        node = symbols_operators[char](recursive_make_node(left), recursive_make_node(right))
+        return node
+            
+        
+    root = recursive_make_node(string)
+    return Problem(root)
+                
+    
+
+
+problem = make_problem('3+2')
+
 print(problem)
-
-problem.use_operator(problem.root.right)
-print(problem)
-
-problem.use_operator(problem.root.left.left)
-print(problem)
-
-problem.use_operator(problem.root.left)
-print(problem)
-
-problem.use_operator(problem.root)
-print(problem)
-
-#problem.print_tree()
-
